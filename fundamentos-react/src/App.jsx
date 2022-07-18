d5cdeb0f46056d8dbca677312c9aa3f48739bd52
Isaac Daniel <isaacbusiness2019@hotmail.com>
2022-07-18 18:50:17 -0300

    finalizando a criação dos cards

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,33 +1,35 @@
 import React from 'react';
-
 import Primeiro from "./components/basicos/Primeiro";
 import ComParametro from "./components/basicos/ComParametro";
 import Fragmento from './components/basicos/Fragmento';
 import Aleatorio from './components/basicos/Aleatorio';
 import Card from './components/layout/Card';
+import "./app.css";
 
 export default function App() {
     return (
-        <div>            
-            <Card titulo="#01 - Primeiro Componente">
-                <Primeiro />
-            </Card>
+        <div className="app">
+            <div className="cards">
+                <Card titulo="#01 - Primeiro Componente" color="#F2E205">
+                    <Primeiro />
+                </Card>
 
-            <Card titulo="#02 - Com Parâmetro">
-                <ComParametro
-                    titulo="Segundo Componente"
-                    subtitulo="Muito Legal!"
-                />
-            </Card>
-            
-            <Card titulo="#03 - Fragmento">
-                <Fragmento />
-            </Card>
-            
-            <Card titulo="#04 - Desafio Aleatório">
-                <Aleatorio valorMin={10} valorMax={20} />
-                <Aleatorio valorMin={10} valorMax={20} />
-            </Card>
+                <Card titulo="#02 - Com Parâmetro" color="#F207E3">
+                    <ComParametro
+                        titulo="Segundo Componente"
+                        subtitulo="Muito Legal!"
+                    />
+                </Card>
+                
+                <Card titulo="#03 - Fragmento" color="#4641D9">
+                    <Fragmento />
+                </Card>
+                
+                <Card titulo="#04 - Desafio Aleatório" color="#D907B6">
+                    <Aleatorio valorMin={10} valorMax={20} />
+                    <Aleatorio valorMin={10} valorMax={20} />
+                </Card>
+            </div>            
         </div>
     );
 }
--- fundamentos-react/src/components/layout/Card.jsx
+++ fundamentos-react/src/components/layout/Card.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import "./card.css";
 
-export default function Card({ titulo, children }) {
+export default function Card({ titulo, color, children }) {
+
+    const cardStyle = {
+        backgroundColor: color || "#F00",
+        borderColor: color || "#F00"
+    }
+
     return (
-        <div className="card">
+        <div className="card" style={cardStyle}>
             <div className="title">
                 { titulo }
             </div>
